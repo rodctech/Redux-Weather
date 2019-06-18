a4b824d64ec8d656489e479dcd517c1d352af913
Brian Espinosa <bespinosa1931@gmail.com>
2019-06-18 11:57:11 -0500

    Pulls selected data in seperate container

--- src/components/WeatherDetail.js
+++ src/components/WeatherDetail.js
@@ -7,17 +7,11 @@ import {
 	onDaySelect
 } from "../actions";
 class WeatherDetail extends Component {
-	componentDidUpdate() {
-		console.log("I Updated");
-		// const renderDays = this.props.weather.selectedDay;
-		// const weather = this.props.weather.weatherData;
-
-		// const afternoon = weather.filter(e => {
-		// 	return e.number === renderDays[0];
-		// });
-	}
+	componentDidUpdate() {}
 
 	render() {
+		const renderDays = this.props.weather.selectedDay;
+		const weather = this.props.weather.weatherData;
 		if (!this.props.weather.selectedDay) {
 			return (
 				<div className="ui container segment">
@@ -25,45 +19,23 @@ class WeatherDetail extends Component {
 				</div>
 			);
 		}
-		const weather = this.props.weather.weatherData;
-		console.log(weather);
-
+		const dayWeather = weather.filter(e => {
+			return e.number === renderDays[0];
+		});
+		const nightWeather = weather.filter(e => {
+			return e.number === renderDays[1];
+		});
 		return (
 			<div className="ui container">
 				<div className="ui segments">
 					<div className="ui segment">
-						<h2>Day</h2>
-						<p style={{ paddingLeft: "1.5em" }}>
-							TIME
-							<span style={{ paddingLeft: "5em" }}>
-								DESCRIPTION
-							</span>
-							<span style={{ paddingLeft: "5em" }}>HIGH</span>
-							<span style={{ paddingLeft: "5em" }}>LOW</span>
-						</p>
+						<h2>{dayWeather[0].name}</h2>
+						<p>{dayWeather[0].detailedForecast}</p>
 					</div>
+					<div className="ui secondary segment">
+						<h2>{nightWeather[0].name}</h2>
 
-					<div className="ui segments">
-						<div className="ui segment">
-							<p>
-								5:00 pm
-								<span style={{ paddingLeft: "5em" }}>
-									Sunny
-								</span>{" "}
-								<span style={{ paddingLeft: "7.5em" }}>
-									75˚
-								</span>
-								<span style={{ paddingLeft: "7.5em" }}>
-									60˚
-								</span>
-							</p>
-						</div>
-						<div className="ui segment">
-							<p>6:00 pm</p>
-						</div>
-						<div className="ui segment">
-							<p>7:00 pm</p>
-						</div>
+						<p>Secondary Content</p>
 					</div>
 				</div>
 			</div>
