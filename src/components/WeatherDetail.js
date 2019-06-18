08a0ce78f724e250f91e79829a0c0c37d485b516
Felipe <ninofelipe73@gmail.com>
2019-06-18 12:21:14 -0500

    finsished weather app

--- src/components/WeatherDetail.js
+++ src/components/WeatherDetail.js
@@ -1,55 +1,68 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import {
-	fetchWeather,
-	fetchLocation,
-	getLocalWeather,
-	onDaySelect
+  fetchWeather,
+  fetchLocation,
+  getLocalWeather,
+  onDaySelect
 } from "../actions";
 class WeatherDetail extends Component {
-	componentDidUpdate() {}
+  componentDidUpdate() {}
 
-	render() {
-		const renderDays = this.props.weather.selectedDay;
-		const weather = this.props.weather.weatherData;
-		if (!this.props.weather.selectedDay) {
-			return (
-				<div className="ui container segment">
-					To seedetailed forcast please click on a day...
-				</div>
-			);
-		}
-		const dayWeather = weather.filter(e => {
-			return e.number === renderDays[0];
-		});
-		const nightWeather = weather.filter(e => {
-			return e.number === renderDays[1];
-		});
-		return (
-			<div className="ui container">
-				<div className="ui segments">
-					<div className="ui segment">
-						<h2>{dayWeather[0].name}</h2>
-						<p>{dayWeather[0].detailedForecast}</p>
-					</div>
-					<div className="ui secondary segment">
-						<h2>{nightWeather[0].name}</h2>
-
-						<p>Secondary Content</p>
-					</div>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    const renderDays = this.props.weather.selectedDay;
+    const weather = this.props.weather.weatherData;
+    if (!this.props.weather.selectedDay) {
+      return (
+        <div className="ui container segment">
+          To seedetailed forcast please click on a day...
+        </div>
+      );
+    }
+    const dayWeather = weather.filter(e => {
+      return e.number === renderDays[0];
+    });
+    const nightWeather = weather.filter(e => {
+      return e.number === renderDays[1];
+    });
+    return (
+      <div className="ui container">
+        <div className="ui segments">
+          <div className="ui segment">
+            <img
+              className="ui centered small circular image"
+              src={dayWeather[0].icon}
+              alt="icon"
+            />
+            <h2>{dayWeather[0].name}</h2>
+            <p>Description: {dayWeather[0].shortForecast}</p>
+            <p>Temperature: {dayWeather[0].temperature} </p>
+            <p>Wind Speed: {dayWeather[0].windSpeed}</p>
+          </div>
+          <div className="ui secondary segment">
+            <img
+              className="ui centered small circular image"
+              src={nightWeather[0].icon}
+              alt="icon"
+            />
+            <h2>{nightWeather[0].name}</h2>
+            <p>Description: {nightWeather[0].shortForecast}</p>
+            <p>Temperature: {nightWeather[0].temperature} </p>
+            <p>Wind Speed: {nightWeather[0].windSpeed}</p>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = state => {
-	return {
-		weather: state.weatherReducer
-	};
+  return {
+    weather: state.weatherReducer
+  };
 };
 
 export default connect(
-	mapStateToProps,
-	{ fetchWeather, fetchLocation, getLocalWeather, onDaySelect }
+  mapStateToProps,
+  { fetchWeather, fetchLocation, getLocalWeather, onDaySelect }
 )(WeatherDetail);
