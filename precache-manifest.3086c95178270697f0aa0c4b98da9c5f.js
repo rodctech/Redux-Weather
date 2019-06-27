1ff61dc0a2a7111b73b8d3ee139a8382ab82381e
Rodrigo Chavez <rodctech@gmail.com>
2019-06-27 11:22:43 -0500

    Updates

---
+++ precache-manifest.3086c95178270697f0aa0c4b98da9c5f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64c1d4702c2e16b9382ef4aa90459eda",
+    "url": "/Redux-Weather/index.html"
+  },
+  {
+    "revision": "5a71394f7925a3951048",
+    "url": "/Redux-Weather/static/css/main.0a45d2c0.chunk.css"
+  },
+  {
+    "revision": "69871a33039b342d5269",
+    "url": "/Redux-Weather/static/js/2.3177067e.chunk.js"
+  },
+  {
+    "revision": "5a71394f7925a3951048",
+    "url": "/Redux-Weather/static/js/main.7ef959b4.chunk.js"
+  },
+  {
+    "revision": "882ab4f125bf6ed9bb4c",
+    "url": "/Redux-Weather/static/js/runtime~main.eb74b77d.js"
+  }
+]);
